0716c1ae2c505a708f7f8d1591bcdb91295175bd
Alicetair Cyric Toyoca <alestaircyrilculanag.coyoca@cit.edu>
2024-06-02 13:53:16 +0800

    assd

--- welltalk/src/main/java/com/communicators/welltalk/Controller/ReferralController.java
+++ welltalk/src/main/java/com/communicators/welltalk/Controller/ReferralController.java
@@ -1,23 +1,25 @@
 package com.communicators.welltalk.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.communicators.welltalk.Service.ReferralService;
 import com.communicators.welltalk.Entity.ReferralEntity;
+import com.communicators.welltalk.Service.ReferralService;
 
-import java.util.List;
-
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/user/referral")
 public class ReferralController {
