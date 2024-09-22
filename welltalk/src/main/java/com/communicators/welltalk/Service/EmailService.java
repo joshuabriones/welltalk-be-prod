0b8aa2c78a726b641cd926bc44523e54a8e4c38b
keisamae <kheisa.selma@cit.edu>
2024-09-22 19:04:36 +0800

    Edit .gitignore

--- welltalk/src/main/java/com/communicators/welltalk/Service/EmailService.java
+++ welltalk/src/main/java/com/communicators/welltalk/Service/EmailService.java
@@ -12,7 +12,7 @@ public class EmailService {
 
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("welltalk@gmail.com");
+        message.setFrom("kheisa.selma@cit.edu");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
