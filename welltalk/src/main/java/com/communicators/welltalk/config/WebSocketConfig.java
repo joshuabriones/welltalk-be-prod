c1ffc012868ef388bbdda04dcff29b7ae523bd2d
Alicetair Cyric Toyoca <alestaircyrilculanag.coyoca@cit.edu>
2024-09-23 11:51:06 +0800

    bean

--- welltalk/src/main/java/com/communicators/welltalk/config/WebSocketConfig.java
+++ welltalk/src/main/java/com/communicators/welltalk/config/WebSocketConfig.java
@@ -1,59 +1,29 @@
+
+
 package com.communicators.welltalk.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-import com.communicators.welltalk.Service.ChatService;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final ChatService chatService;
-
-    public WebSocketConfig(ChatService chatService) {
-        this.chatService = chatService;
+    @Override
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler(), "/ws").setAllowedOrigins("http://localhost:3000");
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("localhost:3000")
+                .withSockJS();
     }
 
-    @Bean
-    public WebSocketHandler webSocketHandler() {
-        return new WebSocketHandler(chatService);
-    }
 }
-
-// package com.communicators.welltalk.config;
-
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.lang.NonNull;
-// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-// import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-// @Configuration
-// @EnableWebSocketMessageBroker
-// public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-//     @Override
-//     public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
-//         config.enableSimpleBroker("/topic");
-//         config.setApplicationDestinationPrefixes("/app");
-//     }
-
-//     @Override
-//     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
-//         registry.addEndpoint("/ws")
-//                 .setAllowedOrigins("localhost:3000")
-//                 .withSockJS();
-//     }
-
-// }
