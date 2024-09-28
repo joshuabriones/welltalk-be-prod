4f2718039196279b7efaf4189d0c8333aceeacc1
Alicetair Cyric Toyoca <alestaircyrilculanag.coyoca@cit.edu>
2024-09-28 21:43:53 +0800

    setappointmentstatus to ongoing if within appointment time duration

--- welltalk/src/main/java/com/communicators/welltalk/Entity/ChatEntity.java
+++ welltalk/src/main/java/com/communicators/welltalk/Entity/ChatEntity.java
@@ -2,7 +2,6 @@ package com.communicators.welltalk.Entity;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
